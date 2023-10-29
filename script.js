a47ed48d0568fdaab116b5a68a61c38723ffa4bb
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-10-29 15:45:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 const blockIdInput = document.getElementById("block_id");
-  const colorInput = document.getElementById("color_id");
+  const colorInput = document.getElementById("colour_id");
   const changeButton = document.getElementById("change_button");
   const resetButton = document.getElementById("reset_button");
   const gridItems = document.querySelectorAll(".grid-item");
